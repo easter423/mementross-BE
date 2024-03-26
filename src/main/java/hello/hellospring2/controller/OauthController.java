1ff7b41a7346cb817c4656b40a7f2a0d46843525
GunyoungHa <ihs20191663@gmail.com>
2024-03-27 02:29:50 +0900

    Add POST mapping for callback link

--- src/main/java/hello/hellospring2/controller/OauthController.java
+++ src/main/java/hello/hellospring2/controller/OauthController.java
@@ -25,37 +25,16 @@ public class OauthController {
     public String callback(
             @PathVariable(name = "socialLoginType") SocialLoginType socialLoginType,
             @RequestParam(name = "code") String code){
-        log.info(">> 소셜 로그인 API 서버로부터 받은 code :: {}", code);
+        log.info(">> GET: 소셜 로그인 API 서버로부터 받은 code :: {}", code);
         return oauthService.requestAccessToken(socialLoginType, code);
     }
 
-//    @PostMapping("/{socialLoginType}/callback")
-//    public String create(MemberForm form){
-//        Member member=new Member();
-//        member.setName(form.getName());
-//        memberService.join(member);
-//        return "redirect:/";
-//    }
-
-    static class Token {
-        public String getAccess_token() {
-            return access_token;
-        }
-
-        public void setAccess_token(String access_token) {
-            this.access_token = access_token;
-        }
-
-        public String getUser_id() {
-            return user_id;
-        }
-
-        public void setUser_id(String user_id) {
-            this.user_id = user_id;
-        }
-
-        private String access_token;
-        private String user_id;
+    @PostMapping(value = "/{socialLoginType}/callback")
+    public String callback2(
+            @PathVariable(name = "socialLoginType") SocialLoginType socialLoginType,
+            @RequestParam(name = "code") String code){
+        log.info(">> POST: 소셜 로그인 API 서버로부터 받은 code :: {}", code);
+        return oauthService.requestAccessToken(socialLoginType, code);
     }
 }
 
