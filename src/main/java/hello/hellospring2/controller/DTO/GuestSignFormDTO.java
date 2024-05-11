693f5b81f77b3e22004ce8f842ceee4f39762a24
GunyoungHa <ihs20191663@gmail.com>
2024-05-11 14:24:27 +0900

    Social User login api v2.2 (Add Swagger Docs)

--- src/main/java/hello/hellospring2/controller/DTO/GuestSignFormDTO.java
+++ src/main/java/hello/hellospring2/controller/DTO/GuestSignFormDTO.java
@@ -1,9 +1,15 @@
 package hello.hellospring2.controller.DTO;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Data
+@AllArgsConstructor
+@Schema(title = "Signup : 게스트 회원가입/로그인 요청 DTO")
 public class GuestSignFormDTO {
     private String phoneId;
 }
---
+++ src/main/java/hello/hellospring2/service/Result.java
@@ -0,0 +1,25 @@
+package hello.hellospring2.service;
+
+import hello.hellospring2.domain.Member;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Result{
+    private Long id;
+    private String guid;
+    private String instaUsername;
+    private String instaId;
+    private String phoneId;
+    private int status;
+
+    public Result(Member member, int status){
+        this.id = member.getId();
+        this.guid = member.getGuid();
+        this.instaUsername = member.getInstaUsername();
+        this.instaId = member.getInstaId();
+        this.phoneId = member.getPhoneId();
+        this.status = status;
+    }
+}
