57252c48e512adc04c1799241c25b16892890ccb
GunyoungHa <ihs20191663@gmail.com>
2024-06-07 20:05:09 +0900

    fix bug #29

--- src/main/java/hello/hellospring2/HelloSpring2Application.java
+++ src/main/java/hello/hellospring2/HelloSpring2Application.java
@@ -2,8 +2,10 @@ package hello.hellospring2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "hello.hellospring2")
 public class HelloSpring2Application {
 
 	public static void main(String[] args) {
